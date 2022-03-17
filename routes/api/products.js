28cb8ea1c1077f18cf61c4b10a8b56a864e02906
NitinPatel2001 <nitinpatel_co20b5_40@dtu.ac.in>
2022-03-17 21:33:16 +0530

    Product Part

--- routes/api/products.js
+++ routes/api/products.js
@@ -1,3 +1,41 @@
 const route = require('express').Router();
+const Product = require('../../db').product
+
+route.get('/',(req,res)=>{
+
+    Product.findAll()
+        .then((product)=>{
+            res.status(300).send(product)
+        })
+        .catch((err)=>{
+            res.status(600).send({
+                error: "Cannot Display the products",
+            })
+        })
+})
+
+route.post('/',(req,res)=>{
+
+    if(isNaN(req.body.price)){
+        return res.status(700).send({
+            error: "Invalid value"
+        })
+    }
+
+    Product.create({
+        name: req.body.name,
+        manufacturer: req.body.manufacturer,
+        price: parseFloat(req.body.price),
+    })
+        .then((product)=>{
+            res.status(301).send(product)
+        })
+        .catch((err)=>{
+            res.status(501).send({
+                error: "Not able to add"
+            })
+        })
+
+})
 
 exports = module.exports = route
